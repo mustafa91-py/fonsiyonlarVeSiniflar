15ef23dd7ec0fa7168bc828f745c8e59d6f6f0ef
Mustafa2 <Mustafauyar91@gmail.com>
2023-12-24 23:51:04 +0300

    chat GPT ile sorun simdilik duzeldi

--- app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/MainActivity.kt
@@ -2,13 +2,19 @@ package com.mustafauyar.fonksiyonlarvesiniflar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cikarma(10,20)
+
     }
-    fun cikarma(x:Int,y:Int){
+
+    fun cikarma(x: Int, y: Int) {
+        val textView: TextView = findViewById(R.id.textView4)
+        textView.text = "sonuç : ${x-y}"
         println("ilk fonksiyon çalıştı")
     }
 }
