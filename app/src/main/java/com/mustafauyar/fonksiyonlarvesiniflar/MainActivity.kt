c9ddc8ca01f95276d7ae6b4f27ddd8bf2451c851
Mustafa2 <Mustafauyar91@gmail.com>
2023-12-25 13:08:56 +0300

    ders3_24 e kadar eklendi

--- app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/MainActivity.kt
@@ -5,16 +5,29 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         cikarma(10,20)
+        var topla = toplama(18,17)
+        textView4().textSize = 24.0f//ekrana sığması için boyutu değiştir
+        textView4().text = "toplama sunucu : $topla"//toplamanın sonucunu emülatörde göster
 
     }
-
-    fun cikarma(x: Int, y: Int) {
+    fun textView4():TextView{
+        // sürekli aynı widget i çağırmak için oluşturulan textView fonskiyoyunu ekrana yazdırmak için
         val textView: TextView = findViewById(R.id.textView4)
-        textView.text = "sonuç : ${x-y}"
+        return textView
+    }
+    fun cikarma(x: Int, y: Int) {
+        val myText = textView4()
+
+        myText.text = "sonuç : ${x-y}"
         println("ilk fonksiyon çalıştı")
+    }
+    fun toplama(x:Int,y:Int):Int{
+        return x+y
     }
 }
