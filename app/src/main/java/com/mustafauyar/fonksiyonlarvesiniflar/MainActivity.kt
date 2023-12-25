92c822f50fc6fc60ff0ea71900b9f7e08a6f3d1c
Mustafa2 <Mustafauyar91@gmail.com>
2023-12-25 13:34:52 +0300

    ders3_24 ile bÄutton yaptik ve ffonksiyon atadik

--- app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mustafauyar.fonksiyonlarvesiniflar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,16 +11,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         cikarma(10,20)
         var topla = toplama(18,17)
+        var candleToButton2 = 0
         textView4().textSize = 24.0f//ekrana sığması için boyutu değiştir
         textView4().text = "toplama sunucu : $topla"//toplamanın sonucunu emülatörde göster
+        button2Widget().setOnClickListener {
+            if (candleToButton2 == 4){
+                textView4().text = "mum kalmadı !"
+            }
+            else {
+                textView4().text = "alltaki butonun\n"+
+                        "$candleToButton2 mumdur ${candleToButton2+1} mumdur ..."
 
+                candleToButton2 ++//mum sayısını arttır
+            }
+
+        }
     }
     fun textView4():TextView{
         // sürekli aynı widget i çağırmak için oluşturulan textView fonskiyoyunu ekrana yazdırmak için
         val textView: TextView = findViewById(R.id.textView4)
         return textView
+    }
+    fun button2Widget():View{
+        val button2:View = findViewById(R.id.button2)
+        return button2
     }
     fun cikarma(x: Int, y: Int) {
         val myText = textView4()
@@ -29,5 +47,9 @@ class MainActivity : AppCompatActivity() {
     }
     fun toplama(x:Int,y:Int):Int{
         return x+y
+    }
+    fun degistir(view:View){
+        val toplamaSonucu = toplama(50,10)
+        textView4().text = "toplama sonucu : $toplamaSonucu"
     }
 }
