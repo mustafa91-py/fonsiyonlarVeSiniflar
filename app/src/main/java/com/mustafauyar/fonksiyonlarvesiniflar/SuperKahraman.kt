abf8564b8c20a6a9134ee487d47ff80f39fdd3da
Mustafa2 <Mustafauyar91@gmail.com>
2023-12-25 15:06:16 +0300

    ders3_26 constructor

--- app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/SuperKahraman.kt
+++ app/src/main/java/com/mustafauyar/fonksiyonlarvesiniflar/SuperKahraman.kt
@@ -1,7 +1,15 @@
 package com.mustafauyar.fonksiyonlarvesiniflar
 
-class SuperKahraman {
+class SuperKahraman(var isim:String="",var meslek:String="",var yas:Int=0) {
+    /*
     var isim = ""
     var meslek = ""
     var yas:Int=0
+    constructor(isim_:String="",meslek_:String="",yas_:Int=0){
+        isim = isim_;meslek=meslek_;yas=yas_
+        this.isim = isim_
+        this.yas = yas_
+        this.meslek = meslek_//bu pythondaki self e benziyor this yapısı
+    }
+     */
 }
